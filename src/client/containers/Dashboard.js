4f9bf7775133858d627f064fc0ad9d828c4fac26
Derek Sakamoto <dmsakamoto@gmail.com>
2016-04-08 16:59:56 -0700

    Finished the refactor for the PollsTable

--- src/client/containers/Dashboard.js
+++ src/client/containers/Dashboard.js
@@ -1,8 +1,6 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import moment from 'moment'
-import _ from 'lodash'
 
 import * as actions from '../actions'
 
@@ -10,18 +8,14 @@ import { PollsTable } from '../components'
 
 import './Dashboard.less'
 
-// Seed data
-// let polls = require('../../../lib/data/polls.json')
-
 function select(state){
-  return { polls: state.polls }
+  return { polls: state.polls, responses: state.responses }
 }
 
 class Dashboard extends React.Component {
 
   componentDidMount() {
     this.refreshPolls()
-    this.refreshUsers()
     this.refreshResponses()
   }
 
@@ -33,22 +27,6 @@ class Dashboard extends React.Component {
   refreshResponses() {
     let { dispatch } = this.props
     dispatch(actions.fetchResponses())
-  }
-
-  refreshUsers() {
-    let { dispatch } = this.props
-    dispatch(actions.fetchUsers())
-  }
-
-  renderRow(poll) {
-    return (
-      <tr key={poll.id}>
-        <td>{poll.question}</td>
-        <td>{poll.start}</td>
-        <td>{poll.end}</td>
-        <td>{poll.polls_responded} / {poll.polls_sent}</td>
-      </tr>
-    )
   }
 
   render() {
@@ -68,6 +46,5 @@ class Dashboard extends React.Component {
     )
   }
 }
-
 
 export default connect(select)(Dashboard)
